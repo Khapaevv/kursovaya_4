0b4f4ef3d49be161c1b754c9c17bbf09a1a2d3fa
Vladimir Khapaev <khapaev@mail.ru>
2024-06-05 20:38:26 +0300

    video Vacancy_class.py

--- src/Vacancy_class.py
+++ src/Vacancy_class.py
@@ -4,17 +4,17 @@ class Vacancy:
     """
     vac_title: str
     vac_url: str
-    salary: int
+    salary: str
     currency: str
     requirement: str
 
-    def __init__(self, name, url, salary, requirement, currency):
+    def __init__(self, name, url, salary, requirement):
         """Зарплата проверяется полностью, а вот для валюты такие проверки еще не сделал"""
         self.name = name
         self.url = url
         self.salary = salary
         if type(self.salary) != int:
-            if self.salary != None:
+            if self.salary:
                 if self.salary.get("from") and not self.salary.get("to"):
                     self.salary = self.salary.get("from")
                 elif not self.salary.get("from") and self.salary.get("to"):
@@ -26,48 +26,51 @@ class Vacancy:
             else:
                 self.salary = 0
         self.requirement = requirement
-        self.currency = currency
+        if salary.get("currency"):
+            self.currency = salary.get('currency')
+        else:
+            self.currency = "Валюта не указана"
+
 
     def __repr__(self):
         return (f'Название вакансии: {self.name}\n'
                 f'Ссылка на вакансию: <{self.url}>\n'
                 f'Зарплата: {self.salary}: {self.currency}\n'
-                f'Требования: {self.requirement}\n'
-                f'Валюта: {self.currency}\n')
+                f'Требования: {self.requirement}\n')
 
 
     def __gt__(self, other):
         """Метод сравнения зарплат"""
         if self.salary > other.salary:
-            return self
+            return True
         else:
-            return other
+            return False
 
 
     def __lt__(self, other):
         """Метод сравнения зарплат"""
         if self.salary < other.salary:
-            return self
+            return True
         else:
-            return other
+            return False
 
 
 if __name__ == "__main__":
     klass = Vacancy("python", "https://hh.ru/vacancy/94354526",
                     {'from': 85000, 'to': 100000, 'currency': 'RUR', 'gross': False},
-                    "уметь много программировать", "RUR")
+                    "уметь много программировать")
 
     klass2 = Vacancy("python", "https://hh.ru/vacancy/94354526",
                     {'from': 335000, 'to': 34100000, 'currency': 'RUR', 'gross': False},
-                    "хоть что-то уметь", "RUR")
+                    "хоть что-то уметь")
 
     b = klass.__repr__()
-    # print(b)
-    cc = klass2.__repr__()
+    print(b)
+    # cc = klass2.__repr__()
     # print(cc)
-    dd = klass.__gt__(klass2)
-    print(dd)
-    ee = klass.__lt__((klass2))
-    print(ee)
+    # dd = klass.__gt__(klass2)
+    # print(dd)
+    # ee = klass.__lt__((klass2))
+    # print(ee)
 
 
---
+++ src/for_json_class.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class ForJsonClass(ABC):
+    pass
+
+    def__init__():
+    pass
+
+    def add_json():
+        pass
+
+    def select_json():
+        pass
+
+class AddClass(ForJsonClass):
+    pass
+
+    def __init__(self, name, url, salary, requirement, currency):
+        self.name = name
+        self.url = url
+        self.salary = salary
+        self.requirement = requirement
+        self.currency = currency
+
